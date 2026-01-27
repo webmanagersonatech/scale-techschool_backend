4ca621186cc0a48cd9f48cea3479167a84531bed
vinoth <webdeveloper2@sonatech.ac.in>
2026-01-27 18:20:56 +0530

    push

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = require('./src/app').default;
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
+
