c08a8792477b807dee4690873fcb68d868803924
vinoth <webdeveloper2@sonatech.ac.in>
2026-06-29 18:33:27 +0530

    push

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ import settingsRoutes from './modules/settings/routes';
 import otpRoutes from './modules/otp/routes'
 import { logger } from './middlewares/logger';
 import dotenv from 'dotenv';
-import studentRoutes from './modules/students/routes';
+import studentRoutes from './modules/students/routes'; 
 import cookieParser from 'cookie-parser';
 
 dotenv.config();
@@ -19,7 +19,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-    origin: ['http://localhost:3000', 'http://160.187.54.80:3000'], // frontend URLs
+    origin: ['http://localhost:3000', 'http://160.187.54.80:3000',"https://scale-certifications.vercel.app","http://161.248.37.193:3005"], // frontend URLs
     credentials: true,
 }));
 
@@ -30,7 +30,7 @@ app.use(cookieParser());
 
 app.use(logger);
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
-app.use('/api/students', studentRoutes);
+
 app.use(cors());
 app.use(express.json());
 app.use(logger);
